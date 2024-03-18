a54301b1e40b4e77caa84738ef5f518482bebaa9
Victor Kim <vskimjr@gmail.com>
2024-03-18 09:11:33 -0400

    finished best-time problem

--- leetcode-easy-practice/remove-element.js
+++ leetcode-easy-practice/remove-element.js
@@ -1,7 +1,11 @@
-
+/* Given an integer array nums and an integer val
+Remove all occurences of val in nums.
+Return the numbers of elements in nums which are not equal to val
+*/
 
 function removeElement(nums, val){
   let numsIdx = 0;
+
   for (let num of nums){
     if (num !== val){
       nums[numsIdx] = num;
